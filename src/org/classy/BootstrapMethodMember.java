25e9f0100f74c1937bb1fc4182c36ba41fac55d5
Martin Tuskevicius <martin.tuskevicius@gmail.com>
2016-08-29 04:06:49 -0400

    Simplified bootstrap method representation.

--- src/org/classy/BootstrapMethodMember.java
+++ src/org/classy/BootstrapMethodMember.java
@@ -2,10 +2,7 @@ package org.classy;
 
 public class BootstrapMethodMember {
 
-    public boolean interfaceMethod;
-    public String owner;
-    public String name;
-    public String descriptor;
+    public Reference reference;
     public Object[] arguments;
 
     public BootstrapMethodMember() {
@@ -13,13 +10,7 @@ public class BootstrapMethodMember {
     }
 
     BootstrapMethodMember(PoolItem[] constantPool, Buffer data) {
-        PoolItem handleInformation = constantPool[data.getUnsignedShort()];
-        interfaceMethod = (handleInformation.value == PoolItem.REF_invokeInterface);
-        PoolItem targetInformation = constantPool[(int) handleInformation.longValue];
-        owner = constantPool[constantPool[targetInformation.value].value].stringValue;
-        PoolItem targetTypeInformation = constantPool[(int) targetInformation.longValue];
-        name = constantPool[targetTypeInformation.value].stringValue;
-        descriptor = constantPool[(int) targetTypeInformation.longValue].stringValue;
+        reference = (Reference) Shared.readConstant(constantPool, data.getUnsignedShort());
         int argumentCount = data.getUnsignedShort();
         arguments = new Object[argumentCount];
         for (int i = 0; i < argumentCount; i++) {
