1298ecf3c3a937b078512b803b285a00e2424fbf
Martin Tuskevicius <martin.tuskevicius@gmail.com>
2017-05-26 16:06:37 -0700

    Exposed more specific collection types.

--- src/org/classy/analysis/cfa/BasicBlock.java
+++ src/org/classy/analysis/cfa/BasicBlock.java
@@ -3,23 +3,22 @@ package org.classy.analysis.cfa;
 import org.classy.instructions.Instruction;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 public class BasicBlock {
 
-    final Collection<BasicBlock> successors = new ArrayList<BasicBlock>(2);
+    final List<BasicBlock> successors = new ArrayList<BasicBlock>(2);
     final List<Instruction> instructions = new ArrayList<Instruction>();
 
     BasicBlock() {
 
     }
 
-    public Iterable<BasicBlock> successors() {
+    public List<BasicBlock> successors() {
         return successors;
     }
 
-    public Iterable<Instruction> instructions() {
+    public List<Instruction> instructions() {
         return instructions;
     }
 }
