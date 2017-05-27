f4cb759d03089d1f5e9ad2b9083e2c41d3ff7a09
Martin Tuskevicius <martin.tuskevicius@gmail.com>
2017-05-26 17:24:39 -0700

    Might as well go all the way now...

--- src/org/classy/analysis/cfa/BasicBlock.java
+++ src/org/classy/analysis/cfa/BasicBlock.java
@@ -7,18 +7,10 @@ import java.util.List;
 
 public class BasicBlock {
 
-    final List<BasicBlock> successors = new ArrayList<BasicBlock>(2);
-    final List<Instruction> instructions = new ArrayList<Instruction>();
+    public final List<BasicBlock> successors = new ArrayList<BasicBlock>(2);
+    public final List<Instruction> instructions = new ArrayList<Instruction>();
 
     BasicBlock() {
 
-    }
-
-    public List<BasicBlock> successors() {
-        return successors;
-    }
-
-    public List<Instruction> instructions() {
-        return instructions;
     }
 }
